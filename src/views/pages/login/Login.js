617523dc14fa06a07d1961fdeac1d7d8b45fd22d
Abbas <a.samadani256@gmail.com>
2021-02-09 01:00:32 +0000

    setting login validation with using formik and you

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+
+
 import {
   CButton,
   CCard,
@@ -16,6 +21,11 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
+const validateSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password : yup.string().min(6 ,'password should not be smaller than 6 character').max(16 , 'password should not be longer than 16 character')
+})
+
 const Login = () => {
   return (
     <div className="c-app c-default-layout flex-row align-items-center">
@@ -25,34 +35,80 @@ const Login = () => {
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-user" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput type="text" placeholder="Username" autoComplete="username" />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-lock-locked" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput type="password" placeholder="Password" autoComplete="current-password" />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs="6">
-                        <CButton color="primary" className="px-4">Login</CButton>
-                      </CCol>
-                      <CCol xs="6" className="text-right">
-                        <CButton color="link" className="px-0">Forgot password?</CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
+                  <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={validateSchema}
+                    onSubmit={(values, { setSubmitting }) => {
+                      setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                      }, 400);
+                    }}
+                  >
+                    {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                      /* and other goodies */
+                    }) => (
+                      <CForm onSubmit={handleSubmit}>
+                        <h1>Login</h1>
+                        <p className="text-muted">Sign In to your account</p>
+                        <CInputGroup className="mb-3">
+                          <CInputGroupPrepend>
+                            <CInputGroupText>
+                              <CIcon name="cil-user" />
+                            </CInputGroupText>
+                          </CInputGroupPrepend>
+                          <CInput
+                            type="text"
+                            placeholder="Username"
+                            autoComplete="username"                            
+                            name="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                          />       
+                                             
+                        </CInputGroup>
+                        <div style={{color : 'red' , marginBottom: '8px'}}>
+                          {errors.email && touched.email && errors.email}
+                          </div>
+                        <CInputGroup className="mb-4">
+                          <CInputGroupPrepend>
+                            <CInputGroupText>
+                              <CIcon name="cil-lock-locked" />
+                            </CInputGroupText>
+                          </CInputGroupPrepend>
+                          <CInput
+                            type="password"
+                            placeholder="Password"
+                            autoComplete="current-password"                            
+                            name="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                          />                                                 
+                          <div style={{color : 'red' , marginBottom: '8px'}}>
+                               {errors.password && touched.password && errors.password}
+                            </div>
+                        </CInputGroup>
+                        
+                        <CRow>
+                          <CCol xs="6">
+                            <CButton color="primary" className="px-4" disabled={isSubmitting}>Login</CButton>
+                          </CCol>
+                          <CCol xs="6" className="text-right">
+                            <CButton color="link" className="px-0">Forgot password?</CButton>
+                          </CCol>
+                        </CRow>
+                      </CForm>
+                    )}
+                  </Formik>
                 </CCardBody>
               </CCard>
               <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
